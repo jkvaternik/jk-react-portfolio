96bcb69d20f9fc41a4448ef652fc1c60289d631b
Jaime <jaime.kvaternik17@gmail.com>
2021-01-25 17:37:14 -0500

    Add footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,8 +9,8 @@ const footer = (props) => {
                 <h3 style={{ textAlign: 'center', margin: '0 0 50px 0'}}>want to say hello? let's connect!</h3>
                 <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                     <a href="mailto:jaime.kvaternik17@gmail.com?Subject=Hello%20Jaime">Email</a>
-                    <a href="https://www.linkedin.com/in/jkvaternik/">LinkedIn</a>
-                    <a href="https://s3-us-west-1.amazonaws.com/jaimekvaternik.com/assets/resume.v3.pdf">Resume</a>
+                    <a href="https://www.linkedin.com/in/jkvaternik/" target="_blank">LinkedIn</a>
+                    <a href="https://s3-us-west-1.amazonaws.com/jaimekvaternik.com/assets/resume.v3.pdf" target="_blank">Resume</a>
                 </div>
             </div>
         </footer>
