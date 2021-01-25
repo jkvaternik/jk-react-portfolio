67589dcd532fb295238263567365df26bc233597
Jaime <jaime.kvaternik17@gmail.com>
2021-01-25 17:22:26 -0500

    Edit projects.json

--- src/components/Projects/Project.js/Project.js
+++ src/components/Projects/Project.js/Project.js
@@ -3,8 +3,6 @@ import React from 'react';
 import Skill from './Skill/Skill';
 import styles from './Project.module.css';
 
-import img from '../../../assets/images/kript.png'
-
 const project = (props) => {
     const skills = props.project.skills.map((skill, index) => {
         return <Skill key={skill + index} skill={skill} />;
@@ -13,11 +11,11 @@ const project = (props) => {
     return (
         <div className={styles.Project}>
             <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: props.project.backgroundColor, width: '100%', height: '300px', borderRadius: '10px 10px 0 0', overflow: 'hidden'}}>
-                <img src={props.project.img ? props.project.img : img} alt={props.project.title + ' Image'}></img>
+                <img src={props.project.img} alt={props.project.title + ' Image'}></img>
             </div>
             <div className={styles.Details}>
                 <h5>{props.project.title} <span style={{marginLeft: '20px', color: '#B4B4B4'}}>{props.project.time}</span></h5>
-                <p style={{fontSize: '0.75vw', fontFamily: 'Helvetica'}}>{props.project.desc}</p>
+                <p style={{fontSize: '14px', lineHeight: '17px'}}>{props.project.desc}</p>
                 <div className={styles.Skills}>
                     {skills}
                 </div>
